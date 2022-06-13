b228f1715b4f17598b39a3200bc4b2cfef88dec6
Kelvin Severino <kseveri@outlook.com>
2022-06-13 20:50:28 -0300

    Aula de Condicionais Parte 1

---
+++ Modulo3/1_Condicoes_parte1/ex008.js
@@ -0,0 +1,8 @@
+console.log('O Console funcionou corretamente...')
+
+var velocidade = 51
+console.log(`A Velocidade do carro é ${velocidade}`)
+
+if(velocidade > 50){
+    console.log('Você ultrapassou a velocidade permitida, MULTA!')
+}
